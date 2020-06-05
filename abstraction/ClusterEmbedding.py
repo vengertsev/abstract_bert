53ae3a0a1193135372d0dba92d0506aed15ebd72
Dmitry Vengertsev <vengertsev@gmail.com>
2020-06-05 16:46:33 -0600

    Working version on CPU with hidden space and attention storage.

---
+++ abstraction/ClusterEmbedding.py
@@ -0,0 +1,14 @@
+
+class ClusterEmbedding:
+
+    @staticmethod
+    def merge_embeddings():
+        pass
+
+    @staticmethod
+    def cluster_embeddings():
+        pass
+
+    @staticmethod
+    def get_clusters_stats():
+        pass
