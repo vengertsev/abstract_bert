647b5f0fa92e5a57b8c891a39c0b0eaa33c46ac5
Dmitry Vengertsev <vengertsev@gmail.com>
2020-06-20 17:00:50 -0600

    Added basic dimensionality reduction.

--- abstraction/ClusterEmbedding.py
+++ abstraction/ClusterEmbedding.py
@@ -24,7 +24,29 @@ class ClusterEmbedding:
         save(fout, df_x)
 
     @staticmethod
-    def cluster_embeddings():
+    def reduce_dimension(fname_merged_embedding, fout, sample_size, dim_red_method):
+        df_x = np.load(fname_merged_embedding, mmap_mode='r')
+        x = df_x[0:sample_size, :, :]
+        print(f'sampled input to dim reduction:{x.shape}')
+
+        if dim_red_method == 'pca':
+            x = x.reshape(sample_size, -1)
+            df = ClusterEmbedding._reduce_dim_pca(x)
+            save(fout, df)
+
+    @staticmethod
+    def _reduce_dim_pca(x):
+        pca = PCA(n_components=120)
+        pca.fit(x)
+        print(f'pca.singular_values_={pca.singular_values_}')
+        print(f'pca.singular_values_[0]={pca.singular_values_[0]}')
+        print(f'pca.singular_values_[100]={pca.singular_values_[100]}')
+        pca_keep = 100
+        x2 = PCA(n_components=pca_keep).fit_transform(x)
+        return x2
+
+    @staticmethod
+    def cluster_embeddings(fname_merged_embedding, fout, clustering_method):
         pass
 
     @staticmethod
--- abstraction/cluster.py
+++ abstraction/cluster.py
@@ -4,14 +4,36 @@ sys.path.insert(1, base_folder)
 from abstraction.ClusterEmbedding import ClusterEmbedding
 import os
 
-folder_hidden_states = f'{base_folder}/save/embeddings/'
-fnames_hid_train = [folder_hidden_states + x for x in os.listdir(folder_hidden_states) if ('train' in x) & ('hidden' in x) ]
+# TODO: add logging instead of prints
 
-layer = 0
+folder_hidden_states = f'{base_folder}/save/embeddings/'
+
+# BERT saved hidden space
+fnames_hid_train = [folder_hidden_states + x for x in os.listdir(folder_hidden_states) if ('train' in x) & ('hid' in x)]
+
+n_hid_layers = 12
 bs = 32
 expected_size = (bs, 125, 768)
+sample_size = bs*100  # number of Twitts that are analysed (max is ~13200)
+dim_red_method = 'pca'
+clustering_method = 'dbscan'
 
 # merge embeddings per layer
-for layer in range(0, 12):
+for layer in range(0, n_hid_layers):
     fout = f'{base_folder}/save/merged_embeddings/df_train_hid_{layer}.npy'
     ClusterEmbedding.merge_embeddings(fnames_hid_train, fout, layer, expected_size)
+
+# reduce dimension
+for layer in range(0, n_hid_layers):
+    fname_merged_embedding = f'{base_folder}/save/merged_embeddings/df_train_hid_{layer}.npy'
+    fout = f'{base_folder}/save/clusters/df_{dim_red_method}_{layer}.npy'
+    ClusterEmbedding.reduce_dimension(fname_merged_embedding, fout, sample_size, dim_red_method)
+
+# clustering
+for layer in range(0, n_hid_layers):
+    fname_dim_reduced = f'{base_folder}/save/cluster/df_{dim_red_method}_{layer}.npy'
+    fout = f'{base_folder}/save/clusters/df_{clustering_method}_{layer}.npy'
+    ClusterEmbedding.cluster_embeddings(fname_dim_reduced, fout, clustering_method)
+
+# visualization is done in the app
+
